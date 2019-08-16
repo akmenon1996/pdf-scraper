311742a296d35d7c71531b19e6eb74cc9a00bbeb
akmenon1996 <menon.ab@husky.neu.edu>
2019-08-15 20:19:09 -0400

    "PDF-Scraper"

--- pdf-scraper.py
+++ pdf-scraper.py
@@ -1,10 +1,15 @@
 import PyPDF2 as pypdf
-pdfFile = open('/Users/abhijit/Desktop/code-for-boston/clean-slate/data/Archive/test1.pdf','rb')
-pdfReader = pypdf.PdfFileReader(pdfFile)
-numpages = pdfReader.numPages
-for i in range(numpages):
-    pageObj = pdfReader.getPage(i)
-    a = pageObj.extractText()
-    file1 = open("myfile.txt", "a")  # append mode
-    file1.write(a)
-    file1.close()
+import os
+directory = '/Users/abhijit/Desktop/code-for-boston/clean-slate/data/Archive/'
+for file in os.listdir(directory):
+    if not file.endswith(".pdf"):
+        continue
+    with open(os.path.join(directory,file), 'rb') as pdfFile:  # Changes here
+        pdfReader = pypdf.PdfFileReader(pdfFile)
+        numpages = pdfReader.numPages
+        for i in range(numpages):
+            pageObj = pdfReader.getPage(i)
+            a = pageObj.extractText()
+            file1 = open(f"{file}.txt", "a")  # append mode
+            file1.write(a)
+            file1.close()
